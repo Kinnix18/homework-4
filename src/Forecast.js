c644c24587bfebad956d7533c382554389bdeca7
Kinnix18 <yanchuk137@gmail.com>
2023-04-18 13:04:57 +0300

    added api to forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,7 +1,16 @@
 import React from "react";
 import "./Forecast.css";
+import axios from "axios";
 
 export default function Forecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "ff4e01btd323751oc4afa5a4c3e73777";
+  let apiURLForecast = `https://api.shecodes.io/weather/v1/forecast?query=${props.data.city}&key=${apiKey}&units=metric`;
+  axios.get(apiURLForecast).then(handleResponse);
+
   return (
     <div className="forecast">
       <div className="row">
